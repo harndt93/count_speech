aa0960a1e5792456319d751b5123b6a3e3431e7e
harndt93 <40641129+harndt93@users.noreply.github.com>
2019-04-15 12:11:06 -0300

    timeout
    update code to run for a certain amount of time (minutes) based on user choice

--- range.py
+++ range.py
@@ -7,6 +7,9 @@ import time
 # Import pyttsx3 module
 import pyttsx3
 
+# Import datetime module know 
+#import datetime
+
 # One time initialization
 engine = pyttsx3.init()
 
@@ -17,54 +20,60 @@ br_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS
 def print_input():
     # Get a time input, and decide a voice language
     try:
-        t = int(input("\nHow long do you want me to count?"))
+        t = int(input("\nHow long do you want me to count (in minutes)? "))
     except ValueError:
         print("Sorry, I only accept numbers for that question.")
     else:
-        print("\nSelect a language, E for English or P for Portuguese.")
+        print("\nSelect a language, E for English or P for Portuguese (BR).")
         v = input("E/P: ")
 
-    if v.upper() == 'E': # If v sames as 'E'
+    if v.upper() == 'E': # If v same as E
         # Use female English voice
         engine.setProperty('voice', en_voice_id)
         b = "Thanks, thats all."
         speech_numbers(t,b)
-    elif v.upper() == 'P': # If v same as 'P'
+    elif v.upper() == 'P': # If v same as P
         # Use female Brazilian voice
         engine.setProperty('voice', br_voice_id)
         b = "Obrigada, isto é tudo."
         speech_numbers(t,b)
     else:
-        print("Sorry, I don't recgonize %s as an option." % (v))
+        print("Sorry, I don't recgonize %s as an option." % (v.upper()))
 
 def speech_numbers(t,b):
-    # List 5000 random numbers between 0 and 100.
+    # List 5000 random numbers between 0 and 5000.
     r = random.sample(range(5000), 5000)
 
     # Create an index = 0
     i = 0
-    
-    # While loop to speech numbers
-    while t != 0:
+
+    # Receive minutes
+    t_out = timeout(t)
+
+    while True:
+        #print(datetime.datetime.now())
         print(r[i])
         engine.say(r[i])
         engine.runAndWait()
-        time.sleep(1)
+        time.sleep(1.5)
         i += 1
-        t -= 1
+        if time.time() > t_out:
+            break
     engine.say(b)
     engine.runAndWait()
 
-def shutdown():
-    return False
+def timeout(t):
+    # Calculate a time minute based
+    t_out = time.time() + 60 * t # 60 * t is our time in minutes.
+    return t_out
 
 ### Main ###
-print("##############################################")
-print("#############  Count me to Sleep  ############")
-print("##############################################")
+print("###############################################")
+print("#############  Count me to Sleep  #############")
+print("###############################################")
 
-flag = True
+print_input()
 
-while flag is True:
-    print_input()
-    flag = shutdown()
+print("\n###############################################")
+print("####################  End  ####################")
+print("###############################################")
